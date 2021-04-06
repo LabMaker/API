fff90a62ceacee5c9ba3633d1c9313b41e756ba6
DanyaalA <Danny13653@gmail.com>
2021-04-06 23:57:42 +0100

    Added Logging

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
-import { AppService, TypeTest } from './app.service';
+import { AppService, LogsDto, TypeTest } from './app.service';
 
 @Controller()
 export class AppController {
@@ -14,6 +14,17 @@ export class AppController {
   @HttpCode(204)
   insertId(@Body() body) {
     return this.appService.insertId(body);
+  }
+
+  @Get('logs')
+  getLogs(): LogsDto {
+    return this.appService.getLogs();
+  }
+
+  @Post('log')
+  @HttpCode(204)
+  insertLog(@Body() body) {
+    return this.appService.insertLog(body);
   }
 
   @Get('config')
