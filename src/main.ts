1d414d230c5ba55b7d914ef2fb205cc86e783082
DanA <Danny13653@gmail.com>
2021-08-19 23:16:19 +0100

    Updated Package.json + PORT

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
