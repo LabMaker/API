3a95ac8e5e0489f2cbdd42b2cc6088a546960787
DanA <Danny13653@gmail.com>
2021-08-30 16:35:19 +0100

    Fixed Origin For Cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3001', process.env.FRONT_END_URL],
+    origin: process.env.FRONT_END_URL,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
