1b5387bd64bad17598c8e37c9977ce5a5a315118
DanA <Danny13653@gmail.com>
2021-08-30 19:46:20 +0100

    Enabled Trust Proxy

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as passport from 'passport';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy');
+
   app.use(cookieParser());
   app.enableCors({
     origin: process.env.FRONT_END_URL,
