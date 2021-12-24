5c519cd330bbe1c2d638ee876aff3f31a285db60
Dan <22224922+DNYLA@users.noreply.github.com>
2021-12-24 21:38:07 +0000

    Renamed Variables to match Use

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -47,6 +47,7 @@ export class AuthService {
   async refreshToken(res: Response, req: Request) {
     const token = req.cookies.jid;
     this.logger.log(`Refresh Token - Auth Service ${token}`);
+    this.logger.error(process.env.JWT_SECRET);
 
     if (!token) {
       this.logger.warn(`Empty Token`);
@@ -54,13 +55,13 @@ export class AuthService {
     }
 
     let payload: any = null;
-
     try {
       payload = this.jwtService.verify(token, {
         secret: process.env.JWT_REFRESH_SECRET,
       });
     } catch (err) {
       this.logger.error(err);
+      this.logger.log('Erro Hit?');
       return res.send({ ok: false, accessToken: '' });
     }
 
@@ -99,11 +100,12 @@ export class AuthService {
         type: type,
         tokenVersion: user.tokenVersion,
       },
-      { secret: process.env.JWT_REFRESH_TOKEN, expiresIn: '7d' },
+      { secret: process.env.JWT_REFRESH_SECRET, expiresIn: '7d' },
     );
   }
 
   createAccessToken(user: User, type: string) {
+    this.logger.error(process.env.JWT_SECRET);
     return this.jwtService.sign(
       {
         sub: user.id,
@@ -111,7 +113,7 @@ export class AuthService {
         discriminator: user.discriminator,
         type: type,
       },
-      { secret: process.env.JWT_TOKEN, expiresIn: '15m' },
+      { secret: process.env.JWT_SECRET, expiresIn: '15m' },
     );
   }
 
@@ -121,7 +123,7 @@ export class AuthService {
         sub: '10019',
         type: 'Bot',
       },
-      { secret: process.env.JWT_TOKEN, expiresIn: '1y' },
+      { secret: process.env.JWT_SECRET, expiresIn: '1y' },
     );
   }
 }
